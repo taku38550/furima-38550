7492ec26791f2d2860cdec05b2461b499a8daf0d
taku38550 <taku_s.1028@icloud.com>
2022-12-25 19:09:52 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   validates :nickname           , presence: true
-  validates :email              , presence: true
-  validates :encrypted_password , presence: true
-  validates :family_name        , presence: true
-  validates :first_name         , presence: true
-  validates :family_name_kana   , presence: true
-  validates :first_name_kana    , presence: true
+  validates :email              , presence: true, uniqueness: true
+  validates :password           , presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :family_name        , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name         , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana   , presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
+  validates :first_name_kana    , presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
   validates :birth_day          , presence: true
 end
