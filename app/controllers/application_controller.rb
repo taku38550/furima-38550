1a103f29331b039592bc1d91a9db6537f8283ee5
taku38550 <taku_s.1028@icloud.com>
2022-12-25 12:23:57 +0900

    新規登録の実装2回目

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day]
+      :sign_up, keys: [:nickname, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day]
     )
   end
 end
