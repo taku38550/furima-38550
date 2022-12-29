f43226a60ca64759be9bef2f0481a6a2ca48f218
taku38550 <taku_s.1028@icloud.com>
2022-12-29 14:20:38 +0900

    コメント内容の修正２回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname           , presence: true
-  validates :password           , presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }, length: { minimum: 6 }
+  validates :password           , format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :family_name        , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name         , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :family_name_kana   , presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
