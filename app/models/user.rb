b90c03591d85242157cfdd4f74870cff0fdd671b
taku38550 <taku_s.1028@icloud.com>
2022-12-29 14:08:21 +0900

    コメント内容の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname           , presence: true
-  validates :email              , presence: true, uniqueness: true
   validates :password           , presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }, length: { minimum: 6 }
   validates :family_name        , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name         , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
