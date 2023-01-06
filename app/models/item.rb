292fda3b21f373f07dd26eac64e945b6372af531
taku38550 <taku_s.1028@icloud.com>
2023-01-06 18:14:02 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
+
+  validates :name             , presence: true
+  validates :description      , presence: true
+  validates :category_id      , presence: true
+  validates :status_ig        , presence: true
+  validates :shipping_cost_id , presence: true
+  validates :prefecture_id    , presence: true
+  validates :shipping_date_id , presence: true
+  validates :price            , presence: true
+  validates :image            , presence: true
+
   belongs_to :user
   has_one_attached :image
 end
