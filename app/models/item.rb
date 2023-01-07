2b46ab90ae365a4a344289cd278b286796a55e3b
taku38550 <taku_s.1028@icloud.com>
2023-01-07 13:08:25 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,16 +7,16 @@ class Item < ApplicationRecord
   validates :shipping_cost_id , presence: true, numericality: { other_than: 1, message: "Shipping_cost can't be blank" }
   validates :prefecture_id    , presence: true, numericality: { other_than: 1, message: "Prefecture can't be blank" }
   validates :shipping_date_id , presence: true, numericality: { other_than: 1, message: "Shipping_date can't be blank" }
-  validates :price            , presence: true
+  validates :price            , presence: true, numericality: { greater_than_or_equal_to: 300, less_than_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
   validates :image            , presence: true
 
   belongs_to :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :status
-  belongs_to :shipping_cost
-  belongs_to :prefecture
-  belongs_to :shipping_date
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_date
 end
