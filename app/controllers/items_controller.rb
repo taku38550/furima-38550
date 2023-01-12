2f8b6b61a794ba201f75e269d5077554f400f0ae
taku38550 <taku_s.1028@icloud.com>
2023-01-12 14:07:36 +0900

    コメント内容の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,9 +33,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.destroy
-      redirect_to root_path
-    end
+    item.destroy
+    redirect_to root_path
   end
 
   private
