407cea5f76583e39cb397e01dec89270a746749d
taku38550 <taku_s.1028@icloud.com>
2023-01-12 13:50:03 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,6 +31,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
+  end
+
   private
 
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create, :show, :edit, :update] do
+  resources :items do
     # resources :purchase_records, only: [:index, :create] 
   end
 end
