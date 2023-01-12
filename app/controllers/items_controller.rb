1fb5b6ff89bdcb59d063360c2b0c6bbf4ca02d36
taku38550 <taku_s.1028@icloud.com>
2023-01-12 14:14:36 +0900

    コメント内容の修正2回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
@@ -32,8 +32,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
